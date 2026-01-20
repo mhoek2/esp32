0ef4d061012935c69c14e490924fd03d2d231d96
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2026-01-20 13:08:23 +0100

    Add simple GPIO blinking led using FreeRTOS Task

--- main/main.c
+++ main/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#define LED_GPIO 8
+#define INTERVAL 500 / portTICK_PERIOD_MS
+
+static uint8_t s_led_state = 0;
+
+static void blink_led(void)
+{
+    gpio_set_level(LED_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+}
 
 void app_main(void)
 {
+    configure_led();
 
+    while (1) {
+        blink_led();
+
+        s_led_state = !s_led_state;
+        vTaskDelay(INTERVAL);
+    } 
 }
