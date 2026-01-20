58ff646c7796db3f4ee0c0cade17a7a8347d669e
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2026-01-20 14:10:22 +0100

    Add simple webserver

--- main/main.c
+++ main/main.c
@@ -6,10 +6,56 @@
 
 #include "wifi.h"
 
+
+#include "esp_http_server.h"
+
 #define LED_GPIO 8
 #define INTERVAL 500 / portTICK_PERIOD_MS
 
 static uint8_t s_led_state = 0;
+
+
+
+
+
+
+
+
+
+static esp_err_t root_get_handler(httpd_req_t *req)
+{
+    const char *html = "<!DOCTYPE html>"
+                       "<html>"
+                       "<head><title>ESP32 AP</title></head>"
+                       "<body>"
+                       "<h1>Hello from ESP32!</h1>"
+                       "<p>Connected via AP</p>"
+                       "</body>"
+                       "</html>";
+
+    httpd_resp_send(req, html, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+//static const char *TAG_HTTP = "HTTP_SERVER";
+static httpd_uri_t root = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = root_get_handler,
+    .user_ctx  = NULL
+};
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &root);
+    }
+
+    return server;
+}
 
 static void blink_led(void)
 {
@@ -27,6 +73,8 @@ void app_main(void)
     configure_led();
     init_wifi_ap();
     
+    start_webserver();
+
     while (1) {
         blink_led();
 
