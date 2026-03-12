00e66c3cb242dfa1c190cd5704ac534187791216
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2026-03-12 13:57:13 +0100

    PHPSim: Extend by unifying the endpoint handler with the c implementation using struct: httpd_uri_t #8

--- main/spiffs_storage/web/index.php
+++ main/spiffs_storage/web/index.php
@@ -1,11 +1,42 @@
 <?php
-$step = 0;
-
+// step/page
+$step = 1;
 $steps = [
     "sta_setup.html",
     "server_setup.html",
     "overview.html",
 ];
+
+class httpd_uri_t {
+    public $uri; 
+    protected $method; 
+    protected $handler; 
+
+    public function __construct($_uri, $_method, $_handler) {
+        $this->uri      = $_uri;
+        $this->method   = $_method;
+        $this->handler  = $_handler;
+    }
+
+    public function run() {
+        call_user_func( $this->handler );
+    }
+}
+
+$uri_handlers = [];
+
+function root_get_handler()
+{
+    global $step;
+    global $steps;
+
+    // most basic way to locally serve a webpage that is stored on the SoC
+    // NOTE: This does not simulate xhr traffic
+    if ( array_key_exists( $step, $steps ) && is_file( $steps[$step] ) ) {
+        include $steps[$step];
+        exit();
+    }
+}
 
 // simulate xhr requests locally
 // keep syntax as similar to the firmware written in c for clarity
@@ -35,13 +66,15 @@ function xhr_set_server_ip( $post )
     httpd_resp_send( $response );
 }
 
-function xhr_handler( $post )
+function &xhr_handler()
 {
-    try {
-        // retrieve the operation mode
-        if ( empty( $post['mode'] ) )
-            throw new Exception('mode keyword is missing in POST data');
+    // retrieve the operation mode
+    if ( empty( $_POST['mode'] ) )
+        throw new Exception('mode keyword is missing in POST data');
 
+    $post = &$_POST;
+
+    try {
         $mode = $post['mode'];
 
         // operation mode dispatching
@@ -66,17 +99,64 @@ function xhr_handler( $post )
     }
 }
 
-// simulate xhr requests
-if ( !empty($_POST) ) {
-    xhr_handler( $_POST );
+function &get_factory_reset_handler ()
+{
+    $response = "{\"data\":{}, \"status\":{\"flag\":\"success\", \"message\":\"Device is reset\"}}";
+
+    httpd_resp_set_type("application/json");
+    httpd_resp_send( $response );
 }
 
-// most basic way to locally serve a webpage that is stored on the SoC
-// NOTE: This does not simulate xhr traffic
-if ( array_key_exists( $step, $steps ) && is_file( $steps[$step] ) ) {
-    include $steps[$step];
-    exit();
+function &reboot_device_handler()
+{
+    $response = "{\"data\":{}, \"status\":{\"flag\":\"success\", \"message\":\"Device is rebooting\"}}";
+
+    httpd_resp_set_type("application/json");
+    httpd_resp_send( $response );
 }
-   
+
+function &disable_ap_handler()
+{
+    $response = "{\"data\":{}, \"status\":{\"flag\":\"success\", \"message\":\"AP is disabling\"}}";
+
+    httpd_resp_set_type("application/json");
+    httpd_resp_send( $response );
+}
+
+function ADD_URI_HANDLER( $_method, $_uri, $_handler, &$count )
+{
+    global $uri_handlers;
+
+    $uri_handlers[$count++] = new httpd_uri_t(
+        $_uri,
+        $_method,
+        $_handler
+    );
+}
+
+function define_endpoints()
+{
+    $num_uri_handlers = 0;
+
+    ADD_URI_HANDLER( "_GET", "/",               "root_get_handler",             $num_uri_handlers );
+    ADD_URI_HANDLER( "_GET", "/xhr",            "xhr_handler",                  $num_uri_handlers );
+    ADD_URI_HANDLER( "_GET", "/factory_reset",  "get_factory_reset_handler",    $num_uri_handlers );
+    ADD_URI_HANDLER( "_GET", "/reboot_device",  "reboot_device_handler",        $num_uri_handlers );
+    ADD_URI_HANDLER( "_GET", "/disable_ap",     "disable_ap_handler",           $num_uri_handlers );
+}
+
+define_endpoints();
+
+// handle endpoints
+if ( !empty($_ENV) ) {
+    foreach ( $uri_handlers as $handler )
+    {
+        if ( $handler->uri === $_ENV['REQUEST_URI'] )
+        {
+            $handler->run();
+        }
+    }
+}
+
 die("Invalid page");
 ?>
